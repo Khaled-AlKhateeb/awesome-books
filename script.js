d00b51a303b87b26b81e2aef42ab68af19b3ae55
Khaled-AlKhateeb <khaled.w.khateeb85@gmail.com>
2022-09-20 16:59:35 +0300

    fix linter errors

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-const addBook = document.getElementById("book");
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const addBook = document.getElementById('book');
 
 const dataMod = [];
 
@@ -11,21 +11,21 @@ function addNewBook() {
     dataMod.push(book);
     
     let stringVal = titleInput.value;
-    let idString = stringVal.replace(/\s/g, "");
+    let idString = stringVal.replace(/\s/g, '');
     window.localStorage.setItem(idString, JSON.stringify(book));
 
-    let bookDiv = document.createElement("div");
-    let bookTitle = document.createElement("h2");
-    let bookAuthor = document.createElement("h2");
-    let removeBtn = document.createElement("button");
-    let divider = document.createElement("hr");
-    removeBtn.innerHTML = "Remove";
-    removeBtn.setAttribute('onclick', "removeBook(this.id)");
+    let bookDiv = document.createElement('div');
+    let bookTitle = document.createElement('h2');
+    let bookAuthor = document.createElement('h2');
+    let removeBtn = document.createElement('button');
+    let divider = document.createElement('hr');
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.setAttribute('onclick', 'removeBook(this.id)');
     bookTitle.innerHTML = titleInput.value;
     bookAuthor.innerHTML = authorInput.value;
     
     bookDiv.setAttribute('id', idString);
-    removeBtn.setAttribute('id', idString + "Rmv");
+    removeBtn.setAttribute('id', idString + 'Rmv');
 
     bookDiv.appendChild(bookTitle);
     bookDiv.appendChild(bookAuthor);
@@ -38,7 +38,7 @@ function removeBook(id) {
     for (let i = 0; i < addBook.children.length; i++) {
         let selected = addBook.children[i]; 
         
-        if (selected.id + "Rmv" === id) {
+        if (selected.id + 'Rmv' === id) {
             addBook.removeChild(selected);
             dataMod.splice(i,1);
             window.localStorage.removeItem(selected.id);
